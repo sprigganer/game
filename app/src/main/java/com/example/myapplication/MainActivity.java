14ce34f7a2f1621e71f6c8d36347b2add417b56f
xSpr1ggan <71220808+xSpr1ggan@users.noreply.github.com>
2020-09-20 12:58:16 +0300

    update 1.1.1.1.1.1.1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-
+protected WebView game;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //init
+        game =(WebView) findViewById(R.id.game);
+
+        //setting
+
+        //load
+
+        game.loadUrl("file:///android_asset/game/index.html");
     }
 }
