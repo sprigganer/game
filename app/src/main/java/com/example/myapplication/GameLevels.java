75fb14de6e447162c7bb199db06587b9cec536a0
xSpr1ggan <71220808+xSpr1ggan@users.noreply.github.com>
2020-09-27 15:39:59 +0300

    новая версия с кнопкой назад

--- app/src/main/java/com/example/myapplication/GameLevels.java
+++ app/src/main/java/com/example/myapplication/GameLevels.java
@@ -1,8 +1,11 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,5 +19,19 @@ public class GameLevels extends AppCompatActivity {
         Window w=getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        Button button_back = (Button)findViewById(R.id.button_back);
+       button_back.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               //здесь будет команда для кнопки
+               try {
+                   Intent intent = new Intent(GameLevels.this, MainActivity.class);
+                startActivity(intent);finish();
+               }
+               catch (Exception e){
+
+               }
+           }
+       });
     }
 }
